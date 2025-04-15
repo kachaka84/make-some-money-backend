e813c7e3d736892ccdd2a3e5e476433c7f78b85f
kachaka84 <hristovachev@abv.bg>
2025-04-15 18:56:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,9 @@ load_dotenv()
 app = FastAPI()
 
 API_KEY = os.getenv("API_FOOTBALL_KEY")
-API_URL = "https://api-football-v1.p.rapidapi.com/v3/fixtures?date=2025-04-15"
+API_URL = "https://google.com"
 
-headers = {
-    "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
-    "x-rapidapi-key": API_KEY
-}
+headers = {}
 
 @app.get("/matches")
 def get_matches():
