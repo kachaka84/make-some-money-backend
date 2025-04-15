e771a002fa204555e3e00c8f8c0f8a58b7c5010e
kachaka84 <hristovachev@abv.bg>
2025-04-15 19:17:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/matches")
+def get_matches():
+    return {
+        "fixtures": [
+            {"home": "Левски", "away": "ЦСКА", "date": "2025-04-16"},
+            {"home": "Реал Мадрид", "away": "Барселона", "date": "2025-04-16"},
+            {"home": "Байерн", "away": "Борусия Дортмунд", "date": "2025-04-16"}
+        ]
+    }
+}
